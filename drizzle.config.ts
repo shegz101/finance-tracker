cb3732846b2c5052769040d23cf5f89e03d721a6
Bobate Olusegun <bobatesegun3gmail.com>
2024-08-27 18:45:24 +0100

    fixed-env-db-url-issue

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
+import './envConfig';
 import { defineConfig } from 'drizzle-kit'
 
-const databaseUrl = process.env.NEXT_PUBLIC_DATABASE_URL;
+const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
   throw new Error('DATABASE_URL environment variable is not set');
