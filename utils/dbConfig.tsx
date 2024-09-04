bd1f9b323d6ef7fb6850b54c9b7528461ba7a94d
Bobate Olusegun <bobatesegun3gmail.com>
2024-09-04 11:41:04 +0100

    added-db-url

--- utils/dbConfig.tsx
+++ utils/dbConfig.tsx
@@ -2,11 +2,7 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-const databaseUrl = process.env.NEXT_PUBLIC_DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error('NEXT_PUBLIC_DATABASE_URL environment variable is not set');
-}
+const databaseUrl = process.env.NEXT_PUBLIC_DATABASE_URL || "postgresql://neondb_owner:RZqo7bO4fetD@ep-summer-heart-a53s5s4u.us-east-2.aws.neon.tech/Expense-Tracker?sslmode=require";
 
 const sql = neon(databaseUrl);
 export const db = drizzle(sql, { schema });
