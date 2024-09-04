a521d4a1048c920057a9a5f40da728c20c1db26e
Bobate Olusegun <bobatesegun3gmail.com>
2024-09-04 12:26:22 +0100

    ---

--- utils/dbConfig.tsx
+++ utils/dbConfig.tsx
@@ -2,7 +2,13 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-const databaseUrl =process.env.NEXT_PUBLIC_DATABASE_URL || "";
+const databaseUrl = process.env.DATABASE_URL || "";
+
+if (!databaseUrl) {
+  throw new Error("No Database connection string was provided to `neon()`.");
+}
+
+console.log("Database URL:", databaseUrl); // Optional: For debugging
 
 const sql = neon(databaseUrl);
 export const db = drizzle(sql, { schema });
